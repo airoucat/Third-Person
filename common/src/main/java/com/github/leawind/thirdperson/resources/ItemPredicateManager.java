2f05b4838d502df2430b953522b2be1ceeec85a6
Leawind <leawind@yeah.net>
2026-04-01 09:18:50 +0800

    chore: update dependency version
    - update com.github.Leawind:inventory-java
    
    (cherry picked from commit 066927fab8dc965f7085424af9bc048352c585d2)

--- common/src/main/java/com/github/leawind/thirdperson/resources/ItemPredicateManager.java
+++ common/src/main/java/com/github/leawind/thirdperson/resources/ItemPredicateManager.java
@@ -118,7 +118,7 @@ public class ItemPredicateManager extends SimpleJsonResourceReloadListener {
     if (ItemPredicateUtil.isInitialized()) {
       reparseImmediately();
     } else {
-      ItemPredicateUtil.ON_INITIALIZED.setOnce(
+      ItemPredicateUtil.ON_INITIALIZED.once(
           "Item predicates in resource", e -> reparseImmediately());
     }
   }
